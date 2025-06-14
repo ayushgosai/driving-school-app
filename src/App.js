d67994a16d9a772d45aa5f3be51bdb7b09e73105
ayushgoswami <ayushgosai913@gmail.com>
2025-06-14 17:21:13 +0530

    Simplify App.js and build files

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import React from 'react';
 
 const App = () => {
   return (
-    <Router basename='/driving-school-app'>
-      <div className="App">
-        <table>
-          <thead>
-            <tr>
-              <th>LL Time</th>
-              <th>Learning License</th>
-              <th>Name</th>
-              <th>Mobile Number</th>
-              <th>Application Number</th>
-              <th>Birth Day</th>
-              <th>DL License</th>
-              <th>Money</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>12:00</td>
-              <td>12345</td>
-              <td>राम</td>
-              <td>9876543210</td>
-              <td>DL12345</td>
-              <td>15/06/1990</td>
-              <td>23456</td>
-              <td>5000</td>
-            </tr>
-            <tr>
-              <td>13:30</td>
-              <td>67890</td>
-              <td>श्याम</td>
-              <td>9876543211</td>
-              <td>DL67890</td>
-              <td>20/07/1992</td>
-              <td>78901</td>
-              <td>6000</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </Router>
+    <div className="App">
+      <table>
+        <thead>
+          <tr>
+            <th>LL Time</th>
+            <th>Learning License</th>
+            <th>Name</th>
+            <th>Mobile Number</th>
+            <th>Application Number</th>
+            <th>Birth Day</th>
+            <th>DL License</th>
+            <th>Money</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>12:00</td>
+            <td>12345</td>
+            <td>राम</td>
+            <td>9876543210</td>
+            <td>DL12345</td>
+            <td>15/06/1990</td>
+            <td>23456</td>
+            <td>5000</td>
+          </tr>
+          <tr>
+            <td>13:30</td>
+            <td>67890</td>
+            <td>श्याम</td>
+            <td>9876543211</td>
+            <td>DL67890</td>
+            <td>20/07/1992</td>
+            <td>78901</td>
+            <td>6000</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
